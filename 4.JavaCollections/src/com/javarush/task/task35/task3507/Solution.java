eb718d14724ff83bfda39fa391aac20220e9b219
Quintessencion <zzerrorzz@list.ru>
2017-02-19 22:06:24 +0300

    Merge branch 'master' of https://github.com/Quintessencion/JavaRushTasks
    # Conflicts:
    #	4.JavaCollections/src/com/javarush/task/task35/task3512/Solution.java

--- 4.JavaCollections/src/com/javarush/task/task35/task3507/Solution.java
+++ 4.JavaCollections/src/com/javarush/task/task35/task3507/Solution.java
@@ -79,15 +79,11 @@ public class Solution {
         InputStream is = new FileInputStream(new File(path));
 
         long length = new File(path).length();
-        if (length > Integer.MAX_VALUE) {
-
-        }
 
         byte[] bytes = new byte[(int) length];
         int offset = 0;
         int numRead = 0;
-        while (offset < bytes.length
-                && (numRead = is.read(bytes, offset, bytes.length - offset)) >= 0) {
+        while (offset < bytes.length && (numRead = is.read(bytes, offset, bytes.length - offset)) >= 0) {
             offset += numRead;
         }
         if (offset < bytes.length) {
