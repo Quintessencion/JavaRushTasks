66f5e1e1077ba9585b7ad7c6f1e699526e3a760b
Quintessencion <zzerrorzz@list.ru>
2017-02-19 13:47:48 +0300

    Merge branch 'master' of https://github.com/Quintessencion/JavaRushTasks
    # Conflicts:
    #	4.JavaCollections/src/com/javarush/task/task35/task3512/Solution.java

--- 4.JavaCollections/src/com/javarush/task/task35/task3507/Solution.java
+++ 4.JavaCollections/src/com/javarush/task/task35/task3507/Solution.java
@@ -77,25 +77,22 @@ public class Solution {
 
     private static byte[] fetchClassFromFS(String path) throws FileNotFoundException, IOException {
         InputStream is = new FileInputStream(new File(path));
-        // Get the size of the file
+
         long length = new File(path).length();
         if (length > Integer.MAX_VALUE) {
-            // File is too large
+
         }
-        // Create the byte array to hold the data
+
         byte[] bytes = new byte[(int) length];
-        // Read in the bytes
         int offset = 0;
         int numRead = 0;
         while (offset < bytes.length
                 && (numRead = is.read(bytes, offset, bytes.length - offset)) >= 0) {
             offset += numRead;
         }
-        // Ensure all the bytes have been read in
         if (offset < bytes.length) {
             throw new IOException("Could not completely read file " + path);
         }
-        // Close the input stream and return bytes
         is.close();
         return bytes;
     }
