6836ec203042c2f37b86e1872ba0ab1399104407
Quintessencion <zzerrorzz@list.ru>
2017-02-23 23:55:57 +0300

    Merge branch 'master' of https://github.com/Quintessencion/JavaRushTasks
    # Conflicts:
    #	4.JavaCollections/src/com/javarush/task/task35/task3512/Solution.java

---
+++ 4.JavaCollections/src/com/javarush/task/task36/task3601/Controller.java
@@ -0,0 +1,12 @@
+package com.javarush.task.task36.task3601;
+
+import java.util.List;
+
+public class Controller {
+
+    private Model model = new Model();
+
+    public List<String> onDataListShow() {
+        return model.getStringDataList();
+    }
+}
---
+++ 4.JavaCollections/src/com/javarush/task/task36/task3601/View.java
@@ -0,0 +1,10 @@
+package com.javarush.task.task36.task3601;
+
+public class View {
+
+    private Controller controller = new Controller();
+
+    public void fireEventShowData() {
+        System.out.println(controller.onDataListShow());
+    }
+}
