410ab06b76fe5436e076a3228f565c723d4a64a8
Quintessencion <zzerrorzz@list.ru>
2017-02-26 00:42:58 +0300

    Merge branch 'master' of https://github.com/Quintessencion/JavaRushTasks
    # Conflicts:
    #	4.JavaCollections/src/com/javarush/task/task35/task3512/Solution.java

--- 4.JavaCollections/src/com/javarush/task/task32/task3210/Solution.java
+++ 4.JavaCollections/src/com/javarush/task/task32/task3210/Solution.java
@@ -8,7 +8,24 @@ import java.io.RandomAccessFile;
 */
 
 public class Solution {
-    public static void main(String... args) {
+    public static void main(String... args) throws IOException {
+        String fileName = args[0];
+        int number = Integer.parseInt(args[1]);
+        String text = args[2];
+        byte[] data = new byte[text.length()];
 
+        RandomAccessFile raf = new RandomAccessFile(fileName, "rw");
+        raf.seek(number);
+        raf.read(data, 0, text.length());
+
+        raf.seek(raf.length());
+
+        if (new String(data).equals(text)) {
+            raf.write("true".getBytes());
+        } else {
+            raf.write("false".getBytes());
+        }
+
+        raf.close();
     }
 }
