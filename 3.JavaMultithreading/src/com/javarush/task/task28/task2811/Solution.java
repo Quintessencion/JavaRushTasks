ea3dd3e7c405b7647dd22b84cee0bd0bf9b9322b
Quintessencion <zzerrorzz@list.ru>
2017-02-28 13:33:52 +0300

    Merge branch 'master' of https://github.com/Quintessencion/JavaRushTasks
    # Conflicts:
    #	4.JavaCollections/src/com/javarush/task/task35/task3512/Solution.java

---
+++ 3.JavaMultithreading/src/com/javarush/task/task28/task2811/Solution.java
@@ -0,0 +1,20 @@
+package com.javarush.task.task28.task2811;
+
+/* 
+ReentrantReadWriteLock
+
+Класс ReadWriteMap должен предоставлять корректный доступ к объекту Map из многих потоков, но в его реализации были допущены ошибки.
+Найди их и исправь.
+
+P.S. Для корректной работы Map в многопоточной среде, желательно пользоваться стандартной реализацией,
+например ConcurrentHashMap из пакета java.util.concurrent. Но иногда требуется работа с другим типом мапы,
+например LinkedHashMap и тогда предложенное решение будет как никогда кстати.
+*/
+
+import java.util.LinkedHashMap;
+
+public class Solution {
+    public static void main(String[] args) {
+        ReadWriteMap<Integer, String> linkedSafeMap = new ReadWriteMap<>(new LinkedHashMap<>());
+    }
+}
