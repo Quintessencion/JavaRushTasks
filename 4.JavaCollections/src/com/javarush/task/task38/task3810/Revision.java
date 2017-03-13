13458272981a9d1d1b7970319050cc78ed8ba26d
Quintessencion <zzerrorzz@list.ru>
2017-03-13 23:19:59 +0300

    Quintessencion/JavaRushTasks4JavaCollections/src/com/javarush/task/task35/task3512/Solution.java

---
+++ 4.JavaCollections/src/com/javarush/task/task38/task3810/Revision.java
@@ -0,0 +1,9 @@
+package com.javarush.task.task38.task3810;
+
+public @interface Revision {
+    //напиши свой код
+    int revision();
+    Date date();
+    String comment() default "";
+    Author[] authors() default {};
+}
