a8c8310a57c5ac1950c48ac8d836aa9faf343cb0
Quintessencion <zzerrorzz@list.ru>
2017-03-14 22:46:09 +0300

    Quintessencion/JavaRushTasks4JavaCollections/src/com/javarush/task/task35/task3512/Solution.java

---
+++ 4.JavaCollections/src/com/javarush/task/task38/task3811/Ticket.java
@@ -0,0 +1,22 @@
+package com.javarush.task.task38.task3811;
+
+import java.lang.annotation.ElementType;
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Target;
+
+@Target(ElementType.TYPE)
+@Retention(RetentionPolicy.RUNTIME)
+public @interface Ticket {
+    String createdBy() default "Amigo";
+
+    String[] tags() default {};
+
+    Priority priority() default Priority.MEDIUM;
+
+    public enum Priority {
+        LOW,
+        MEDIUM,
+        HIGH
+    }
+}
