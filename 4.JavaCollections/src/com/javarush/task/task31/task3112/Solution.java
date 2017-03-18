0cbf2648408a6ec1f3e427443c4a4deff1921932
Quintessencion <zzerrorzz@list.ru>
2017-03-18 23:29:54 +0300

    Quintessencion/JavaRushTasks4JavaCollections/src/com/javarush/task/task35/task3512/Solution.java

--- 4.JavaCollections/src/com/javarush/task/task31/task3112/Solution.java
+++ 4.JavaCollections/src/com/javarush/task/task31/task3112/Solution.java
@@ -1,7 +1,5 @@
 package com.javarush.task.task31.task3112;
 
-import javax.imageio.ImageIO;
-import java.awt.image.BufferedImage;
 import java.io.*;
 import java.net.URL;
 import java.nio.file.Files;
