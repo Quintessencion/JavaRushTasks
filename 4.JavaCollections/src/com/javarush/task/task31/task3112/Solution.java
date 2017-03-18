efb49a130cd4e5403eed57248b22c7bc4017dcc8
Quintessencion <zzerrorzz@list.ru>
2017-03-18 23:27:24 +0300

    Quintessencion/JavaRushTasks4JavaCollections/src/com/javarush/task/task35/task3512/Solution.java

---
+++ 4.JavaCollections/src/com/javarush/task/task31/task3112/Solution.java
@@ -0,0 +1,50 @@
+package com.javarush.task.task31.task3112;
+
+import javax.imageio.ImageIO;
+import java.awt.image.BufferedImage;
+import java.io.*;
+import java.net.URL;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+
+/* 
+Загрузчик файлов
+
+Реализуй метод downloadFile(String urlString, Path downloadDirectory),
+на вход которого подается ссылка для скачивания файла и папка, куда нужно закачать файл.
+Все ссылки имеют вид:
+https://yastatic.net/morda-logo/i/citylogos/yandex19-logo-ru.png
+http://toogle.com/files/rules.txt
+https://pacemook.com/photos/image1.jpg
+
+Метод должен создать объект URL и загрузить содержимое файла на локальный диск.
+Выкачивай сначала во временную директорию, чтобы в случае неуспешной загрузки в твоей директории не оставались недокачанные файлы.
+Затем перемести файл в пользовательскую директорию. Имя для файла возьми из ссылки.
+Используй только классы и методы из пакета java.nio.
+*/
+public class Solution {
+
+    public static void main(String[] args) throws IOException {
+//        Path passwords = downloadFile("https://www.amigo.com/ship/secretPassword.txt", Paths.get("D:/MyDownloads"));
+        Path passwords = downloadFile("https://yastatic.net/morda-logo/i/citylogos/yandex19-logo-ru.png", Paths.get("D:/MyDownloads"));
+
+        for (String line : Files.readAllLines(passwords)) {
+            System.out.println(line);
+        }
+    }
+
+    public static Path downloadFile(String urlString, Path downloadDirectory) throws IOException {
+        // implement this method
+        URL url = new URL(urlString);
+        InputStream inputStream = url.openStream();
+
+        Path tmp = Files.createTempFile("temp-", ".tmp");
+        Files.copy(inputStream, tmp);
+        String fileName = urlString.substring(urlString.lastIndexOf("/"));
+        String dir = downloadDirectory.toString();
+        Path moveTo = Paths.get(dir + fileName);
+        Files.move(tmp, moveTo);
+        return Paths.get(downloadDirectory.toString() + fileName);
+    }
+}
