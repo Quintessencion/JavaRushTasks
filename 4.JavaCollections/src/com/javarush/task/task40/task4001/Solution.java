82ead3325cd109bd65904af9e1312d2d54089c97
Quintessencion <zzerrorzz@list.ru>
2017-03-23 23:46:04 +0300

    Quintessencion/JavaRushTasks4JavaCollections/src/com/javarush/task/task35/task3512/Solution.java

---
+++ 4.JavaCollections/src/com/javarush/task/task40/task4001/Solution.java
@@ -0,0 +1,51 @@
+package com.javarush.task.task40.task4001;
+
+import java.io.BufferedReader;
+import java.io.DataOutputStream;
+import java.io.InputStreamReader;
+import java.io.OutputStream;
+import java.net.HttpURLConnection;
+import java.net.URL;
+import java.nio.charset.StandardCharsets;
+
+/* 
+POST, а не GET
+
+Исправь ошибки в методе sendPost, чтобы он отправлял POST-запрос с переданными параметрами.
+Примечание: метод main в тестировании не участвует, но чтобы программа корректно работала локально,
+можешь зайти на сайт http://requestb.in/, создать свой RequestBin и использовать его в main.
+*/
+
+public class Solution {
+    public static void main(String[] args) throws Exception {
+        Solution solution = new Solution();
+        solution.sendPost(new URL("http://requestb.in/tajca9ta"), "name=zapp&mood=good&locale=&id=777");
+    }
+
+    public void sendPost(URL url, String urlParameters) throws Exception {
+        HttpURLConnection connection = (HttpURLConnection) url.openConnection();
+
+        connection.setRequestMethod("POST");
+        connection.setDoOutput(true);
+        connection.setRequestProperty("User-Agent", "Mozilla/5.0");
+
+        connection.setDoOutput(true);
+        try (DataOutputStream dos = new DataOutputStream(connection.getOutputStream())) {
+            dos.write(urlParameters.getBytes(StandardCharsets.UTF_8));
+        }
+
+        int responseCode = connection.getResponseCode();
+        System.out.println("Response Code: " + responseCode);
+
+        BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(connection.getInputStream()));
+        String responseLine;
+        StringBuilder response = new StringBuilder();
+
+        while ((responseLine = bufferedReader.readLine()) != null) {
+            response.append(responseLine);
+        }
+        bufferedReader.close();
+
+        System.out.println("Response: " + response.toString());
+    }
+}
