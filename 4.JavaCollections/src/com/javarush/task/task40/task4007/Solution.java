9c3d2bd7edb9cece65b8714b44275c93f753187e
Quintessencion <zzerrorzz@list.ru>
2017-03-26 16:18:00 +0300

    Quintessencion/JavaRushTasks4JavaCollections/src/com/javarush/task/task35/task3512/Solution.java

--- 4.JavaCollections/src/com/javarush/task/task40/task4007/Solution.java
+++ 4.JavaCollections/src/com/javarush/task/task40/task4007/Solution.java
@@ -21,6 +21,9 @@ public class Solution {
 
     public static void printDate(String date) throws ParseException {
         //напишите тут ваш код
+        boolean day = false;
+        boolean hour = false;
+
         Calendar calendar = Calendar.getInstance();
         Pattern p = Pattern.compile("(\\d{1,2}).(\\d{1,2}).(\\d{4})\\s(\\d{1,2}):(\\d{1,2}):(\\d{1,2})");
         Matcher m = p.matcher(date);
@@ -32,24 +35,19 @@ public class Solution {
         if (m.find()) {
             SimpleDateFormat sdf = new SimpleDateFormat("d.M.yyyy H:m:s");
             calendar.setTime(sdf.parse(date));
-
-            System.out.println("День: " + calendar.get(Calendar.DAY_OF_MONTH));
-            System.out.println("День недели: " + ((calendar.get(Calendar.DAY_OF_WEEK) - 1) == 0 ? 7 : calendar.get(Calendar.DAY_OF_WEEK) - 1));
-            System.out.println("День месяца: " + calendar.get(Calendar.DAY_OF_MONTH));
-            System.out.println("День года: " + calendar.get(Calendar.DAY_OF_YEAR));
-            System.out.println("Неделя месяца: " + calendar.get(Calendar.WEEK_OF_MONTH));
-            System.out.println("Неделя года: " + calendar.get(Calendar.WEEK_OF_YEAR));
-            System.out.println("Месяц: " + (calendar.get(Calendar.MONTH) + 1));
-            System.out.println("Год: " + calendar.get(Calendar.YEAR));
-            System.out.println("AM или PM: " + (calendar.get(Calendar.HOUR_OF_DAY) > 12 ? "PM" : "AM"));
-            System.out.println("Часы: " + calendar.get(Calendar.HOUR));
-            System.out.println("Часы дня: " + calendar.get(Calendar.HOUR_OF_DAY));
-            System.out.println("Минуты: " + calendar.get(Calendar.MINUTE));
-            System.out.println("Секунды: " + calendar.get(Calendar.SECOND));
+            day = true;
+            hour = true;
         } else if (m2.find()) {
             SimpleDateFormat sdf = new SimpleDateFormat("d.M.yyyy");
             calendar.setTime(sdf.parse(date));
+            day = true;
+        } else if (m3.find()) {
+            SimpleDateFormat sdf = new SimpleDateFormat("H:m:s");
+            calendar.setTime(sdf.parse(date));
+            hour = true;
+        }
 
+        if (day) {
             System.out.println("День: " + calendar.get(Calendar.DAY_OF_MONTH));
             System.out.println("День недели: " + ((calendar.get(Calendar.DAY_OF_WEEK) - 1) == 0 ? 7 : calendar.get(Calendar.DAY_OF_WEEK) - 1));
             System.out.println("День месяца: " + calendar.get(Calendar.DAY_OF_MONTH));
@@ -58,10 +56,8 @@ public class Solution {
             System.out.println("Неделя года: " + calendar.get(Calendar.WEEK_OF_YEAR));
             System.out.println("Месяц: " + (calendar.get(Calendar.MONTH) + 1));
             System.out.println("Год: " + calendar.get(Calendar.YEAR));
-        } else if (m3.find()) {
-            SimpleDateFormat sdf = new SimpleDateFormat("H:m:s");
-            calendar.setTime(sdf.parse(date));
-
+        }
+        if (hour) {
             System.out.println("AM или PM: " + (calendar.get(Calendar.HOUR_OF_DAY) > 12 ? "PM" : "AM"));
             System.out.println("Часы: " + calendar.get(Calendar.HOUR));
             System.out.println("Часы дня: " + calendar.get(Calendar.HOUR_OF_DAY));
